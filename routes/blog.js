401c4db452d07a2897b1a84dc3fce9c5e8f806eb
sagites <chizzyikemba@gmail.com>
2024-10-27 00:08:17 +0100

    Updated routes

--- routes/blog.js
+++ routes/blog.js
@@ -1,9 +1,47 @@
+// const express = require('express');
+// const router = express.Router();
+// const {getAllBlogs,createBlog,getBlog,updateBlog,deleteBlog} = require('../controllers/blogs');
+
+
+// router.route('/').get(getAllBlogs).post(createBlog);
+// router.route('/:id').get(getBlog).patch(updateBlog).delete(deleteBlog);
+
+// module.exports = router
+
 const express = require('express');
+const {
+  getAllBlogs,
+  createBlog,
+  getBlog,
+  updateBlog,
+  deleteBlog,
+  likeBlog,
+  dislikeBlog,
+  filterBlogs,
+  searchBlogs,
+} = require('../controllers/blogs');
+const { protect } = require('../middleware/auth');
 const router = express.Router();
-const {getAllBlogs,createBlog,getBlog,updateBlog,deleteBlog} = require('../controllers/blogs');
 
+router.route('/')
+  .get(getAllBlogs)
+  .post(protect, createBlog);
 
-router.route('/').get(getAllBlogs).post(createBlog);
-router.route('/:id').get(getBlog).patch(updateBlog).delete(deleteBlog);
+router.route('/:id')
+  .get(getBlog)
+  .put(protect, updateBlog)
+  .delete(protect, deleteBlog);
 
-module.exports = router
+router.route('/:id/like')
+  .post(protect, likeBlog);
+
+router.route('/:id/dislike')
+  .post(protect, dislikeBlog);
+
+router.route('/filter')
+  .get(filterBlogs);
+
+router.route('/search')
+  .get(searchBlogs);
+
+module.exports = router;
---
+++ routes/commentRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { createComment, getComments } = require('../controllers/commentController');
+const { protect } = require('../middleware/auth');
+const router = express.Router();
+
+router.route('/:id/comments')
+  .post(protect, createComment)
+  .get(getComments);
+
+module.exports = router;
